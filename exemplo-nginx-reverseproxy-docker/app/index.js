e006c80ea559265e59c5ad971db8c4b864d0e159
Lucas Santos <lhs.santoss@gmail.com>
2018-02-17 17:20:59 -0200

    Standard

--- exemplo-nginx-reverseproxy-docker/app/index.js
+++ exemplo-nginx-reverseproxy-docker/app/index.js
@@ -1,20 +1,19 @@
-var express = require('express'),
-    http = require('http'),
-    redis = require('redis');
+const express = require('express')
+const http = require('http')
+const redis = require('redis')
+var app = express()
+var client = redis.createClient('6379', 'redis')
 
-var app = express();
- 
-var client = redis.createClient('6379', 'redis');
+app.get('/', function (req, res, next) {
+  // Logamos o servidor que estamos no load balancer
+  console.log(`Receiving request from: ${process.env.CONTAINER} `)
+  // Incrementamos o contador
+  client.incr('counter', function (err, counter) {
+    if (err) return next(err)
+    res.send(`Container: ${process.env.CONTAINER} - This page has been viewed ${counter} times!`)
+  })
+})
 
-
-app.get('/', function(req, res, next) {
-  console.log(`Receiving request from: ${process.env.CONTAINER} `);
-  client.incr('counter', function(err, counter) {
-    if(err) return next(err);
-    res.send(`Container: ${process.env.CONTAINER} - This page has been viewed ${ counter }  times!`);
-  });
-});
-
-http.createServer(app).listen(process.env.PORT || 8080, function() {
-  console.log('Listening on port ' + (process.env.PORT || 8080));
-});
+http.createServer(app).listen(process.env.PORT || 8080, function () {
+  console.log('Listening on port ' + (process.env.PORT || 8080))
+})
