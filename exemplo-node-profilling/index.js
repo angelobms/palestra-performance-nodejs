7750ea3d4e5be45954ea1a6794a9dad9dfcd490d
Lucas Santos <lhs.santoss@gmail.com>
2018-02-18 16:22:58 -0300

    Corrige erro com exemplo

--- exemplo-node-profilling/index.js
+++ exemplo-node-profilling/index.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 
 function hash (password) {
   const salt = crypto.randomBytes(128).toString('base64')
-  const hash = crypto.pbkdf2Sync(password, salt, 10000, 512)
+  const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512')
   return hash
 }
 
